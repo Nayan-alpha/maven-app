91b8a8b035ea9c9d1db76bd838cf58161451f56b
Nayan-alpha <149183073+Nayan-alpha@users.noreply.github.com>
2023-12-20 10:21:03 +0530

    1

---
+++ src/main/java/com/mavenweb/maven/App.java
@@ -0,0 +1,13 @@
+package com.mavenweb.maven;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
